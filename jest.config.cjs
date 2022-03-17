936f9f4c79a2613c17922fef04a4b1c84b6550e2
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2022-03-17 20:52:29 +0800

    Jest sequential (#2)
    * feat(esbuild): removed nodemon, used tsc esbuild watch mode
    
    * test(jest): use sequential testing and esbuild to transpile test code instead of parallel and babel

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,5 @@
 module.exports = {
     verbose: true,
-    transform: {
-        '^.+\\.(ts)$': 'ts-jest',
-        '^.+\\.(js)$': 'babel-jest',
-    },
     transformIgnorePatterns: [],
     testTimeout: 43200,
     coveragePathIgnorePatterns: ['node_modules'],
