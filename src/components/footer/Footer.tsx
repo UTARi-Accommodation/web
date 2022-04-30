04e828e87dc975e213ee6ff326bce088629e7476
GervinFung <gervinfungdaxuen@gmail.com>
2022-04-30 14:15:01 +0800

    chore(copyright-notice): added year range

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -31,70 +31,82 @@ const LinkToContent = ({
     );
 };
 
-const Footer = () => (
-    <Container>
-        <InnerContainer>
-            <SummaryContainer>
-                <ColumnSummary>
-                    <SummaryTitle>Accommodation Type</SummaryTitle>
-                    <LinkToContent
-                        content="Condominium"
-                        link={condominiumsRoute}
-                    />
-                    <LinkToContent content="House" link={housesRoute} />
-                    <LinkToContent content="Room" link={roomsRoute} />
-                    <LinkToContent
-                        content="Find Roommate"
-                        link={roommatesRoute}
-                    />
-                </ColumnSummary>
-                <ColumnSummary>
-                    <SummaryTitle>Website</SummaryTitle>
-                    <LinkToContent content="Home" link="/" />
-                    <LinkToContent content="About" link="/about" />
-                    <LinkToContent content="Contact" link="/contact" />
-                    <LinkToContent content="How it works" link="/work" />
-                </ColumnSummary>
-                <ColumnSummary>
-                    <SummaryTitle>Developers</SummaryTitle>
-                    <LinkToContent
-                        content="Open Source"
-                        link="https://github.com/Utari-Room/"
-                    />
-                </ColumnSummary>
-                <ColumnSummary>
-                    <SummaryTitle>Social</SummaryTitle>
-                    <LinkToContent
-                        content="Github"
-                        link="https://github.com/Utari-Room/"
-                    />
-                    <LinkToContent
-                        content="Facebook"
-                        link="https://m.facebook.com/GervinFung/"
-                    />
-                    <LinkToContent
-                        content="LinkedIn"
-                        link="https://my.linkedin.com/in/gervin-fung-387409209/"
-                    />
-                    <LinkToContent
-                        content="Instagram"
-                        link="https://www.instagram.com/poolofdeath20/"
-                    />
-                </ColumnSummary>
-            </SummaryContainer>
-            <FooterHozitonalLine />
-            <CopyRightContainer>
-                <CopyRight>
-                    Copyright &copy;{new Date().getFullYear()}
-                </CopyRight>
-                <RowSummary>
-                    <LinkToContent content="Privacy" link="/privacy-policy" />
-                    <LinkToContent content="Terms" link="/terms-conditions" />
-                </RowSummary>
-            </CopyRightContainer>
-        </InnerContainer>
-    </Container>
-);
+const Footer = () => {
+    const originYear = 2022;
+    const year = new Date().getFullYear();
+
+    return (
+        <Container>
+            <InnerContainer>
+                <SummaryContainer>
+                    <ColumnSummary>
+                        <SummaryTitle>Accommodation Type</SummaryTitle>
+                        <LinkToContent
+                            content="Condominium"
+                            link={condominiumsRoute}
+                        />
+                        <LinkToContent content="House" link={housesRoute} />
+                        <LinkToContent content="Room" link={roomsRoute} />
+                        <LinkToContent
+                            content="Find Roommate"
+                            link={roommatesRoute}
+                        />
+                    </ColumnSummary>
+                    <ColumnSummary>
+                        <SummaryTitle>Website</SummaryTitle>
+                        <LinkToContent content="Home" link="/" />
+                        <LinkToContent content="About" link="/about" />
+                        <LinkToContent content="Contact" link="/contact" />
+                        <LinkToContent content="How it works" link="/work" />
+                    </ColumnSummary>
+                    <ColumnSummary>
+                        <SummaryTitle>Developers</SummaryTitle>
+                        <LinkToContent
+                            content="Open Source"
+                            link="https://github.com/Utari-Room/"
+                        />
+                    </ColumnSummary>
+                    <ColumnSummary>
+                        <SummaryTitle>Social</SummaryTitle>
+                        <LinkToContent
+                            content="Github"
+                            link="https://github.com/Utari-Room/"
+                        />
+                        <LinkToContent
+                            content="Facebook"
+                            link="https://m.facebook.com/GervinFung/"
+                        />
+                        <LinkToContent
+                            content="LinkedIn"
+                            link="https://my.linkedin.com/in/gervin-fung-387409209/"
+                        />
+                        <LinkToContent
+                            content="Instagram"
+                            link="https://www.instagram.com/poolofdeath20/"
+                        />
+                    </ColumnSummary>
+                </SummaryContainer>
+                <FooterHozitonalLine />
+                <CopyRightContainer>
+                    <CopyRight>
+                        Copyright &copy;
+                        {year === originYear ? year : `${originYear} - ${year}`}
+                    </CopyRight>
+                    <RowSummary>
+                        <LinkToContent
+                            content="Privacy"
+                            link="/privacy-policy"
+                        />
+                        <LinkToContent
+                            content="Terms"
+                            link="/terms-conditions"
+                        />
+                    </RowSummary>
+                </CopyRightContainer>
+            </InnerContainer>
+        </Container>
+    );
+};
 
 const Container = styled.footer`
     font-family: Montserrat, sans-serif !important;
